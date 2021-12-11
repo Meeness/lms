3ce4f42f1adfdd6d6eec7e148b4508dff012dc71
aivarsyo <aivars.lejnieks@gmail.com>
2021-12-11 06:51:33 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ function start(){
 
 function burger(){
       // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu")
   // On click
   hamburger.addEventListener("click", function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
+    menu.classList.toggle("active")
   });
 }
